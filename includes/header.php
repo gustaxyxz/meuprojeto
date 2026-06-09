349cf8edaaba3f9a980c34b8a65587c24ad057ff
Gus Queiroz <gustavo9925000@gmail.com>
2026-06-09 10:14:26 -0300

    auto-commit

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
         <title>Metalúrgica Oliveira</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet">
     </head>
     <body>
 
